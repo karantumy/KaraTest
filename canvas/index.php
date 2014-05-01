adf032037da84f07ceb8c1e6b906c62991eeb25a
Jamie Salvage <jamie@prettygoodidigtal.com>
2014-05-01 16:43:44 +0100

    FB Meta update

--- canvas/index.php
+++ canvas/index.php
@@ -1,1 +1,17 @@
-<script>window.top.location="https://www.facebook.com/lettsrevision/app_648098001912462"</script>
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Letts Revision</title>
+		<meta type="description" content="What’s your learning style? Complete our quiz to find out and get 25% off our guides online!">
+		<meta property="og:site_name" content="Letts Revision">
+        <meta property="og:type" content="website">
+        <meta property="og:locale" content="en_GB">
+        <meta property="og:title" content="Letts Revision">
+        <meta property="og:description" content="What’s your learning style? Complete our quiz to find out and get 25% off our guides online!">
+        <meta property="og:url" content="https://apps.facebook.com/learningstylesquiz/">
+		<script>window.top.location="https://www.facebook.com/lettsrevision/app_648098001912462"</script>
+	</head>
+	<body>
+	</body>
+</html>
